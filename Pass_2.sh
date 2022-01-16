b347c83190e64722d8268aa4d979958acdcb4a71
Jamie <jamo.marshall+github@gmail.com>
2022-01-16 12:27:07 +0800

    20220116 - reduced apparent errors with ||

--- Pass_2.sh
+++ Pass_2.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
 
-## List the folders available to the user from the current wd
-#ls
-## Prompt for a folder name, cd into that folder, if it does not exist, make it and cd into it.
-#read -p 'Please enter a folder name: ' folder_name &&  cd "$folder_name" || mkdir "$folder_name" && cd "$folder_name" ## now why does it fail to find existing files if the fail condition is set?
-## Prompt the user for a password, if successful, pass the default variable to echo and forward the output to a txt doc.
-#
-#exit 0
-
+## Query the user for a directory name
 read -p 'Please enter a folder name: ' folder_name 
 
-
+## Determine if that directory exists
 if [ -x "$folder_name" ] ; then ## what the hell is the diffrence between -x and -d
-    cd $folder_name
+
+## If that directory exits within the current working directory, navigate to it.
+    cd "$folder_name" || exit 0 
+
+## Prompt the user for a password, pipe to a hash function, direct the output to a file
     read -sp 'Password: ' && echo "$REPLY" | sha256sum > secret.txt
 
 else 
-    mkdir $folder_name && cd $folder_name
+    mkdir "$folder_name" && cd "$folder_name" || exit 0
     read -sp 'Password: ' && echo "$REPLY" | sha256sum > secret.txt
 fi
