6b4dd1387defe979204c13a0da92a722f5c58750
Jamie <jamo.marshall+github@gmail.com>
2022-01-16 11:30:26 +0800

    initial commit

---
+++ Pass_Test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## List the folders available to the user from the current wd
+ls
+## Prompt for a folder name, cd into that folder, if it does not exist, make it and cd into it.
+read -p 'Please enter a folder name: ' folder_name &&  cd "$folder_name" #||echo "File does not exist" ; exit 0 #||mkdir "$folder_name" && cd "$folder_name" ## now why does it fail to find existing files if the fail condition is set?
+## Prompt the user for a password, if successful, pass the default variable to echo and forward the output to a txt doc.
+read -sp 'Password: ' && echo "$REPLY" | sha256sum > secret.txt
+exit 0
